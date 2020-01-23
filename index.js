762606a890320112a5fbdbfa15f5c43f6edb85a5
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 12:06:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    console.log (`This Saturday, I want to ${activity}`)
+}
+
+function mondayWork(activity = "go to the office") {
+    console.log (`This Monday, I will ${activity}`)
+}
+
+let wrapAdjective = function wrapAdjective(what= "*") {
+    return function(whatever= "special") {
+        return `You are ${whatever}`
+    }
+}
