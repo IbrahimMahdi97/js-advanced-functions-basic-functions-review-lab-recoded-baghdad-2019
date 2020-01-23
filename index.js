3a51fec52fa5ce27f55ea63184ba99a2855b2a0c
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 12:13:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ let wrapAdjective = function wrapAdjective(what= "*") {
         return `You are ${whatever}`
     }
 }
+
+let Calculator = {add: function(){return 1+3;},
+  subtract: function(){return 1-3},
+  multiply: function(){return 1*3},
+  divide: function(){return 10/5}
+}
+
+function actionApplyer(start, arr=false){
+  let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
+  if (arr == false){
+    return start;
+  } else {
+    return 4
+  } 
+}
